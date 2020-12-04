04c1c64957127d85c44ede3ad5373801247c7130
Bhavesh Lohana <53433312+bhaveshlohana@users.noreply.github.com>
2020-12-04 19:12:56 +0530

    update

---
+++ forms.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Login V3</title>
+
+    <title>Login</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <link rel="stylesheet" type="text/css" href="css/navbar.css">
+</head>
+
+<body>
+    <div id="site-wrapper">
+        <section id="navbar">
+            <div class="logo-container">
+                <div class="logo"><img src="images/logo1.png"></div>
+            </div>
+            <nav id="nav">
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="menu.php">Menu</a></li>
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">Catering</a></li>
+                    <li><a href="login.php">Login</a></li>
+                </ul>
+            </nav>
+        </section>
+
+        <div class="limiter">
+            <div class="container-form">
+                <div class="wrap-form">
+                    <form class="form-form validate-form">
+
+                        <span class="form-form-title">
+                        Log in
+                    </span>
+
+                        <div class="wrap-field">
+                            <input class="field" type="text" name="username" placeholder="Username">
+                            <span class="focus-field"></span>
+                        </div>
+
+                        <div class="wrap-field">
+                            <input class="field" type="password" name="pass" placeholder="Password">
+                            <span class="focus-field"></span>
+                        </div>
+
+                        <div class="container-form-form-btn">
+                            <button class="form-form-btn">
+                            Login
+                        </button>
+                        </div>
+
+                        <div class="text-center">
+                            <a class="txt1" href="#">
+                            Forgot Password?
+                        </a>
+                        </div>
+                        <div class="text-center">
+                            <a class="txt1" href="#">
+                            Register Now!
+                        </a>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ register.php
@@ -0,0 +1,133 @@
+<?php 
+    
+    $conn=mysqli_connect("localhost", "bhavesh", "test123", "foodorderingsite");
+    if(!$conn) {
+        echo "Connection Error: " . mysqli_connect_error();
+    }
+    
+    if($_SERVER["REQUEST_METHOD"]=='POST'){
+    $fname=$_POST['fname'];
+    $lname=$_POST['lname'];
+    $email=$_POST['email'];
+    $phone=$_POST['number'];
+    $street=$_POST['street'];
+    $city=$_POST['city'];
+    $state=$_POST['state'];
+    $pincode=$_POST['pincode'];
+    $password=$_POST['password'];
+    $cpassword=$_POST['cpassword'];
+    if($password==$cpassword) {
+    
+    $query="INSERT INTO customer (c_fname, c_lname, c_email, c_phone_number, c_street, c_city, c_state, c_pincode, c_password) VALUES('{$fname}', '{$lname}', '{$email}', {$phone}, '{$street}', '{$city}', '{$state}', {$pincode}, '{$password}')";
+    $update=mysqli_query($conn, $query);
+    if(!$update) {
+    echo "ERROR WHILE INSERTING!";
+    }
+
+    }
+    else {
+        echo "Enter same password.";
+    }
+    
+
+    }
+?>
+
+
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Register | Head Over Meals</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <link rel="stylesheet" type="text/css" href="css/navbar.css">
+</head>
+
+<body>
+    <section id="navbar">
+        <div class="logo-container">
+            <div class="logo"><img src="images/logo1.png"></div>
+        </div>
+        <nav id="nav">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="menu.php">Menu</a></li>
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Catering</a></li>
+                <li><a href="login.php">Login</a></li>
+            </ul>
+        </nav>
+    </section>
+    <br><br>
+    <div class="limiter">
+        <div class="container-form">
+            <div class="wrap-form">
+                <form class="form-form validate-form" method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+                    <span class="form-form-title">
+                        Register
+                    </span>
+                    <div class="wrap-field">
+                        <input class="field" type="text" name="fname" placeholder="First Name">
+                        <span class="focus-field"></span>
+                    </div><div class="wrap-field">
+                        <input class="field" type="text" name="lname" placeholder="Last Name">
+                        <span class="focus-field"></span>
+                    </div>
+                    <div class="wrap-field">
+                        <input class="field" type="email" name="email" placeholder="Email Address">
+                        <span class="focus-field"></span>
+                    </div>
+                    <div class="wrap-field">
+                        <input class="field" type="number" name="number" placeholder="Phone Number">
+                        <span class="focus-field"></span>
+                    </div>
+                    <div class="wrap-field">
+                        <input class="field" type="text" name="street" placeholder="Street">
+                        <span class="focus-field"></span>
+                    </div>
+                    <div class="wrap-field">
+                        <input class="field" type="text" name="city" placeholder="City">
+                        <span class="focus-field"></span>
+                    </div>
+                    <div class="wrap-field">
+                        <input class="field" type="text" name="state" placeholder="State">
+                        <span class="focus-field"></span>
+                    </div>
+                    <div class="wrap-field">
+                        <input class="field" type="number" name="pincode" placeholder="Pincode">
+                        <span class="focus-field"></span>
+                    </div>
+                    <div class="wrap-field">
+                        <input class="field" type="password" name="password" placeholder="Password">
+                        <span class="focus-field"></span>
+                    </div>
+                    <div class="wrap-field">
+                        <input class="field" type="password" name="cpassword" placeholder="Confirm Password">
+                        <span class="focus-field"></span>
+                    </div>
+                    <div class="container-form-form-btn">
+                        <button class="form-form-btn">
+                            Register
+                        </button>
+                    </div>
+                    <div class="text-center">
+                        <a class="txt1" href="#">
+                            Forgot Password?
+                        </a>
+                    </div>
+                    <div class="text-center">
+                        <a class="txt1" href="#">
+                            Register Now!
+                        </a>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
