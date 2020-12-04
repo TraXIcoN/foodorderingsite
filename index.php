fc0f507bc9ed5a0ae27cdd9b8c72c617e40a7791
Bhavesh Lohana <53433312+bhaveshlohana@users.noreply.github.com>
2020-12-05 00:53:06 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Head over Meals</title>
+    <!-- Latest compiled and minified CSS -->
+    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script> -->
+    <script src="https://use.fontawesome.com/653db87ebf.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+    <link rel="stylesheet" type="text/css" href="css/navbar.css">
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"> -->
+</head>
+
+<body>
+    <section id="navbar">
+        <div class="logo-container">
+            <div class="logo"><img src="images/logo1.png"></div>
+        </div>
+        <nav id="nav">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="menu.php">Menu</a></li>
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Catering</a></li>
+                <li><a href="login.php">Login</a></li>
+            </ul>
+        </nav>
+    </section>
+
+    <div class="container resize" id="img_container">
+        <img class="caro-img" src="images/caro21.jpg">
+        <button class="btn button"><a href="register.php">Register</a></button>
+    </div>
+    <section id="intro">
+        <div class="wrapper">
+            <div class="intro-description">
+                <h1>Welcome to Head Over Meals</h1>
+                <p>Head Over Meals is the best hassle-free way to order food online. Whether looking for breakfast, lunch, dinner or late night snack, we have it all.</p>
+            </div>
+            <div class="second-svg">
+                <img src="images/s2.svg">
+            </div>
+        </div>
+        <hr>
+        <div class="wrapper">
+            <div class="first-svg">
+                <img src="images/s1.svg">
+            </div>
+            <div class="intro-description">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce laoreet ac tellus sit amet bibendum. Ut ut velit dapibus, tempor massa nec, eleifend odio. Aliquam ligula lorem, feugiat a felis eu, vehicula semper nunc. Vivamus non urna
+                    iaculis, pretium nulla sit amet, efficitur tellus. Vivamus eu enim consectetur justo suscipit maximus. Suspendisse potenti. Pellentesque efficitur est ac efficitur aliquet. Sed lobortis eu neque ac tincidunt. Ut venenatis dictum felis
+                    non venenatis. Sed nisi metus, placerat a neque in, tincidunt interdum magna. Cras eget pretium lorem.</p>
+            </div>
+
+        </div>
+
+    </section>
+    <section id="some-review">
+        <div class="slider">
+            <div class="slides">
+                <div id="slide-1">
+                    "This is the original and authentic Indian restaurant I would like to say original flavour of Indian food service is five star"
+                </div>
+                <div id="slide-2">
+                    "“After a good dinner one can forgive anybody, even one's own relations.”"
+                </div>
+                <div id="slide-3">
+                    "Been coming here since it first opened and have never had any complaints.The food is fantastic and the staff are always friendly and helpful.Chilly Chicken Masala to die for."
+                </div>
+            </div>
+            <a href="#slide-1"> </a>
+            <a href="#slide-2"> </a>
+            <a href="#slide-3"> </a>
+        </div>
+    </section>
+    <footer class="footer">
+        <div class="inner-footer">
+            <div class="about-us">
+                <h3>About</h3>
+                <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce laoreet ac tellus sit amet bibendum. Ut ut velit dapibus, tempor massa nec, eleifend odio. Aliquam ligula lorem, feugiat a felis eu, vehicula semper nunc. Vivamus non urna
+                    iaculis, pretium nulla sit amet, efficitur tellus. </p>
+            </div>
+            <div class="quick-links">
+                <h3>Quick Links</h3>
+                <ul class="footer-links">
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                    <li><a href="#">Contribute</a></li>
+                    <li><a href="#">Privacy Policy</a></li>
+                    <li><a href="#">Sitemap</a></li>
+                </ul>
+            </div>
+            <div class="contact-us">
+                <h3>Contact Us</h3>
+                <ul class="social-icons">
+                    <li><a class="facebook" href="#"><i class="fa fa-facebook"></i>Facebook</a></li>
+                    <li><a class="twitter" href="#"><i class="fa fa-twitter"></i>Twitter</a></li>
+                    <li><a class="linkedin" href="#"><i class="fa fa-linkedin"></i>LinkedIn</a></li>
+                </ul>
+            </div>
+        </div>
+    </footer>
+</body>
+
+</html>
