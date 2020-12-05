3c51c8e5d227c1cbac81073977a2bec894930fde
Aditya Mohan <54040096+TraXIcoN@users.noreply.github.com>
2020-12-05 12:17:27 +0530

    Changed heading

--- index.php
+++ index.php
@@ -89,7 +89,7 @@
         <div class="hero-image caro-">
             <img class="caro-img" src="images/caro21.jpg">
             <div class="hero-text">
-            <h1 style="font-size:50px">WANT TO SELL FOOD!</h1>
+            <h1 style="font-size:50px">ORDER FOOD NOW!</h1>
             <button><a href="register.php">Register</a></button>
         </div>
     </div>
