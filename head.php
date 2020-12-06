bf2d83434b8f8a5f3074c3e92d6ed309da2e9f86
Aditya Mohan <54040096+TraXIcoN@users.noreply.github.com>
2020-12-07 05:20:33 +0530

    Made rough layout of today's specials

--- head.php
+++ head.php
@@ -5,13 +5,11 @@
 
 <link rel="stylesheet" href="css/dropdown.css">
 <link rel="stylesheet" type="text/css" href="css/menu.css">
-    <link rel="stylesheet" type="text/css" href="css/styles.css">
     <link rel="stylesheet" type="text/css" href="css/menu.css">
     <link rel="stylesheet" type="text/css" href="css/main.css">
     <link rel="stylesheet" type="text/css" href="css/cart.css">
-    <link rel="stylesheet" type="text/css" href="css/navbar.css">
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
 
-    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
