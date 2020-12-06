f38fff1c6f8f6906177fcb73420efe7eb364dd94
Aditya Mohan <54040096+TraXIcoN@users.noreply.github.com>
2020-12-06 22:51:00 +0530

    made a common footer file

--- menu.php
+++ menu.php
@@ -86,33 +86,7 @@
 </div>
 </section>
 
-    <footer class="footer">
-        <div class="inner-footer">
-            <div class="about-us">
-                <h3>About</h3>
-                <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce laoreet ac tellus sit amet bibendum. Ut ut velit dapibus, tempor massa nec, eleifend odio. Aliquam ligula lorem, feugiat a felis eu, vehicula semper nunc. Vivamus non urna
-                    iaculis, pretium nulla sit amet, efficitur tellus. </p>
-            </div>
-            <div class="quick-links">
-                <h3>Quick Links</h3>
-                <ul class="footer-links">
-                    <li><a href="#">About Us</a></li>
-                    <li><a href="#">Contact Us</a></li>
-                    <li><a href="#">Contribute</a></li>
-                    <li><a href="#">Privacy Policy</a></li>
-                    <li><a href="#">Sitemap</a></li>
-                </ul>
-            </div>
-            <div class="contact-us">
-                <h3>Contact Us</h3>
-                <ul class="social-icons">
-                    <li><a class="facebook" href="#"><i class="fa fa-facebook"></i>Facebook</a></li>
-                    <li><a class="twitter" href="#"><i class="fa fa-twitter"></i>Twitter</a></li>
-                    <li><a class="linkedin" href="#"><i class="fa fa-linkedin"></i>LinkedIn</a></li>
-                </ul>
-            </div>
-        </div>
-    </footer>
+<?php include('footer.php'); ?>
 
 
 
