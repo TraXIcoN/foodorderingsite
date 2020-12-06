256348a7adec0172980ef904db794a425f9fb17b
Aditya Mohan <54040096+TraXIcoN@users.noreply.github.com>
2020-12-06 23:34:23 +0530

    changed bootstrap cdn

--- head.php
+++ head.php
@@ -1,10 +1,8 @@
 <meta charset="utf-8">
 
 
-<script src="/cdn-cgi/apps/head/OkbNSnEV_PNHTKP2_EYPrFNyZ8Q.js"></script>
 <link rel="shortcut icon" href="img/favicon.ico">
 
-<meta name="viewport" content="width=device-width">
 <link rel="stylesheet" href="css/dropdown.css">
 <link rel="stylesheet" type="text/css" href="css/menu.css">
     <link rel="stylesheet" type="text/css" href="css/styles.css">
@@ -18,7 +16,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
     <link rel="stylesheet" href="css/plugin.css">
     <link rel="stylesheet" href="css/menu_main.css">
