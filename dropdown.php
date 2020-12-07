e708b5627797a4a2c2de9ac537cca3e7ab53ae2f
Aditya Mohan <54040096+TraXIcoN@users.noreply.github.com>
2020-12-07 19:01:15 +0530

    Made profile page

--- dropdown.php
+++ dropdown.php
@@ -4,7 +4,7 @@ if(isset($_SESSION['logged'])) {
 	echo '<div class="dropdown">
   <div class="dropbtn" style="color:white;font-family: Raleway;"><i class="fa fa-user"  style="margin-right:3px; color: white;" aria-hidden="true"></i>'.strtoupper($_SESSION["user_name"]).'</div>
   <div class="dropdown-content">
-  <a href="#">Profile</a>
+  <a href="profile.php">Profile</a>
   <a href="#">Tickets</a>
   <a href="logout.php">Log out</a>
   </div>
