440600e1bab6f070cd457d353bde68287f313e7a
Aditya Mohan <54040096+TraXIcoN@users.noreply.github.com>
2020-12-08 18:55:19 +0530

    Fixed bugs

--- navbar.php
+++ navbar.php
@@ -1,6 +1,6 @@
 
 
-<nav class="navbar navbar-expand-lg navbar-dark fixed-top" style="background-color: black; z-index: 51;">
+<nav class="navbar navbar-expand-lg navbar-dark fixed-top" style="background-color: black; z-index: 60;">
   
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
