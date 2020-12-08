77ab43481e4d431a1f7acb03e5677e4f2b774b8d
Mayank Motwani <mayank.n.motwani@gmail.com>
2020-12-08 13:30:44 +0530

    deleted useless file and modified pages a bit

---
+++ admin/blank.php
@@ -0,0 +1,36 @@
+<?php 
+    include('includes/header.php');
+    
+?>
+
+
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <!-- Main Content -->
+            <div id="content">
+
+                <!-- Topbar -->
+                
+                <!-- End of Topbar -->
+
+                <!-- Begin Page Content -->
+                <div class="container-fluid" style="padding: 100px; text-align: center;">
+
+                    <!-- Page Heading -->
+                    <h1 class="h3 mb-4 text-gray-800">Blank Page</h1>
+
+                </div>
+                <!-- /.container-fluid -->
+
+            </div>
+            <!-- End of Main Content -->
+
+        
+
+
+<?php
+include('includes/scripts.php');
+
+?>
