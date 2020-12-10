6685cc040fb49554d215c7c5eb1aa8f8af43e472
Bhavesh Lohana <53433312+bhaveshlohana@users.noreply.github.com>
2020-12-10 19:12:41 +0530

    Update foodorderingsite.sql

--- database/foodorderingsite.sql
+++ database/foodorderingsite.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 09, 2020 at 03:28 PM
+-- Generation Time: Dec 10, 2020 at 02:41 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.5
 
@@ -67,12 +67,11 @@ CREATE TABLE `customer` (
 --
 
 INSERT INTO `customer` (`c_id`, `c_email`, `c_password`, `c_fname`, `c_lname`, `c_street`, `c_city`, `c_state`, `c_pincode`, `c_phone_number`) VALUES
-(1, '2018.bhavesh.lohana@ves.ac.in', 'bhavesh', 'Bhavesh', 'Lohana', 'Section-20', 'Ulhasnagar', 'Maharashtra', 421002, 9322048502),
+(1, '2018.bhavesh.lohana@ves.ac.in', 'bhave', 'Bhavesh', 'Lohana', 'Section-20', 'Ulhasnagar', 'Maharashtra', 421002, 9322048502),
 (2, 'bhaveshlohana1@gmail.com', 'bhavesh', 'Bhavesh', 'Lohana', 'Section-20, Ulhasnagar', 'Ulhasnagar', 'Maharashtra', 421002, 123456788),
-(3, 'mayank@gmail.com', 'mayank', 'Mayank', 'Motwani', 'Section-20, Ulhasnagar', 'Ulhasnagar', 'Maharashtra', 421002, 2147483647),
-(4, 'aditya@gmail.com', 'aditya', 'Aditya', 'Mohan', 'xyz', 'Chembur', 'Maharashtra', 0, 123456789),
-(5, 'bhaveshlohana11@gmail.com', '12345', 'Bhavesh', 'Lohana', '123', 'Ulhasnagar', 'Maharashtra', 421003, 2147483647),
-(6, 'v@gmail.com', 'abcde', 'Vaibhav', 'Boliya', '1', 'abc', 'maharashtra', 111111, 989896);
+(3, '2018.mayank.motwani@ves.ac.in', 'mayank', 'Mayank', 'Motwani', 'Section-20, Ulhasnagar', 'Ulhasnagar', 'Maharashtra', 421002, 2147483647),
+(4, '2018.aditya.mohan@ves.ac.in', 'aditya', 'Aditya', 'Mohan', 'xyz', 'Chembur', 'Maharashtra', 0, 123456789),
+(5, 'bhaveshlohana11@gmail.com', '12345', 'Bhavesh', 'Lohana', '123', 'Ulhasnagar', 'Maharashtra', 421003, 2147483647);
 
 -- --------------------------------------------------------
 
@@ -92,19 +91,10 @@ CREATE TABLE `delivery` (
 --
 
 INSERT INTO `delivery` (`d_id`, `agent_name`, `agent_number`, `o_id`) VALUES
-(8, 'Jayesh', '9898989898', 20),
-(9, 'Vivek', '9999999999', 21),
-(10, 'Ajay', '9999999999', 22),
 (11, 'Vivek', '9898989898', 23),
 (12, 'Vivek', '9999999999', 24),
 (13, 'Ajay', '9898989898', 25),
-(14, 'Naresh', '1212121212', 26),
-(15, 'Jayesh', '9797989899', 0),
-(16, 'Ajay', '9797989899', 0),
-(17, 'Ajay', '1212121212', 0),
-(18, 'Jayesh', '1212121212', 0),
-(19, 'Vivek', '1212121212', 0),
-(20, 'Jayesh', '1212121212', 0);
+(14, 'Naresh', '1212121212', 26);
 
 -- --------------------------------------------------------
 
@@ -135,7 +125,7 @@ INSERT INTO `food` (`f_id`, `f_name`, `f_price`, `f_description`, `f_special`, `
 (6, 'Chole Bhature', 80, 'Chickpea', 0, 4, 'img\\menu\\2\\chole.jpg'),
 (7, 'Gulab Jamun', 60, 'Gulab Jamun', 0, 5, 'img\\menu\\2\\gulabjamun.jpg'),
 (8, 'Chocolate cake', 90, 'Cake', 1, 5, 'img\\menu\\2\\cm.jpg'),
-(9, 'Cheese Pizza', 20, 'Cheese Pizza', 0, 5, 'img\\menu\\2\\5.jpg');
+(9, 'Cheese Pizza', 20, 'Cheese Pizza', 0, 1, 'img\\menu\\2\\5.jpg');
 
 -- --------------------------------------------------------
 
@@ -160,12 +150,14 @@ CREATE TABLE `orders` (
 
 INSERT INTO `orders` (`o_id`, `c_id`, `o_price`, `o_no_of_items`, `o_status`, `o_time`, `o_address`, `o_special_notes`) VALUES
 (20, 1, 50, 1, 'Undelivered', '2020-12-09 11:43:02', 'Section-20   Ulhasnagar Maharashtra 421002', 'Yes'),
-(21, 1, 110, 1, 'Undelivered', '2020-12-09 11:43:23', 'Section-20   Ulhasnagar Maharashtra 421002', ''),
-(22, 1, 110, 1, 'Undelivered', '2020-12-09 11:49:07', 'Section-20   Ulhasnagar Maharashtra 421002', ''),
-(23, 1, 110, 1, 'Undelivered', '2020-12-09 12:01:04', 'Section-20   Ulhasnagar Maharashtra 421002', ''),
-(24, 1, 110, 1, 'Undelivered', '2020-12-09 12:05:38', 'Section-20   Ulhasnagar Maharashtra 421002', ''),
-(25, 1, 110, 1, 'Undelivered', '2020-12-09 12:07:25', 'Section-20   Ulhasnagar Maharashtra 421002', ''),
-(26, 1, 110, 1, 'Undelivered', '2020-12-09 12:47:45', 'Section-20   Ulhasnagar Maharashtra 421002', '');
+(33, 1, 320, 5, 'Undelivered', '2020-12-09 16:00:20', 'Section-20   Ulhasnagar Maharashtra 421002', ''),
+(34, 4, 1470, 6, 'Undelivered', '2020-12-09 16:07:43', 'xyz   Chembur Maharashtra 0', ''),
+(35, 1, 1450, 3, 'Undelivered', '2020-12-09 16:08:39', 'Section-20   Ulhasnagar Maharashtra 421002', ''),
+(36, 7, 1050, 5, 'Undelivered', '2020-12-09 16:14:12', 's20   UNR Maharashtra 421003', ''),
+(37, 7, 180, 1, 'Undelivered', '2020-12-09 16:16:13', 's20   UNR Maharashtra 421003', ''),
+(38, 1, 1090, 4, 'Undelivered', '2020-12-09 16:45:28', 'Section-20   Ulhasnagar Maharashtra 421002', ''),
+(39, 1, 1020, 4, 'Undelivered', '2020-12-09 16:47:17', 'Section-20   Ulhasnagar Maharashtra 421002', ''),
+(40, 1, 550, 1, 'Undelivered', '2020-12-09 19:15:13', 'On plot, Section-20   Ulhasnagar Maharashtra 421002', 'As fast as possible.');
 
 -- --------------------------------------------------------
 
@@ -185,13 +177,21 @@ CREATE TABLE `order_details` (
 --
 
 INSERT INTO `order_details` (`od_id`, `o_id`, `f_id`, `f_quantity`) VALUES
-(30, 20, 2, 1),
-(31, 21, 5, 1),
-(32, 22, 5, 1),
-(33, 23, 5, 1),
-(34, 24, 5, 1),
-(35, 25, 5, 1),
-(36, 26, 5, 1);
+(45, 33, 2, 1),
+(46, 33, 4, 1),
+(47, 33, 7, 1),
+(48, 33, 8, 1),
+(49, 33, 9, 1),
+(50, 34, 1, 5),
+(51, 34, 4, 5),
+(52, 34, 7, 1),
+(53, 34, 8, 5),
+(54, 34, 9, 1),
+(58, 35, 8, 5),
+(70, 39, 4, 5),
+(71, 39, 8, 5),
+(72, 39, 9, 1),
+(73, 40, 5, 5);
 
 -- --------------------------------------------------------
 
@@ -212,17 +212,11 @@ CREATE TABLE `payment` (
 INSERT INTO `payment` (`p_id`, `o_id`, `p_mode`) VALUES
 (3, 20, 'Online'),
 (4, 21, 'Online'),
-(5, 22, 'Online'),
-(6, 23, 'Online'),
-(7, 24, 'Online'),
-(8, 25, 'Online'),
-(9, 26, 'Online'),
-(10, 0, ''),
-(11, 0, ''),
-(12, 0, ''),
-(13, 0, ''),
-(14, 0, ''),
-(15, 0, '');
+(25, 36, 'COD'),
+(26, 37, 'Online'),
+(27, 38, 'COD'),
+(28, 39, 'COD'),
+(29, 40, 'COD');
 
 -- --------------------------------------------------------
 
@@ -249,7 +243,9 @@ INSERT INTO `review` (`c_id`, `f_id`, `r_id`, `r_rating`, `r_description`, `r_ti
 (2, 2, 3, 2, 'Its nice.', '2020-12-08 19:49:19'),
 (3, 3, 4, 4, 'Very Good.', '2020-12-08 19:50:57'),
 (1, 2, 5, 5, 'Amazing!', '2020-12-09 10:35:03'),
-(1, 1, 6, 1, 'Its good.', '2020-12-09 14:27:09');
+(1, 1, 6, 1, 'Its good.', '2020-12-09 14:27:09'),
+(7, 7, 7, 2, 'Not Good', '2020-12-09 16:13:38'),
+(1, 2, 8, 3, 'Yes', '2020-12-09 16:46:33');
 
 -- --------------------------------------------------------
 
@@ -271,9 +267,7 @@ CREATE TABLE `tickets` (
 INSERT INTO `tickets` (`c_id`, `ticket_id`, `ticket_query`, `ticket_response`) VALUES
 (1, 1, 'What\'s the status?', '\'Unanswered\''),
 (1, 2, 'Any updates?', '\'Unanswered\''),
-(1, 3, 'Do let me know', 'Yes'),
-(1, 4, 'Do let me know', '\'Unanswered\''),
-(6, 5, 'Hello!', '\'Unanswered\'');
+(1, 3, 'Do let me know', 'Yes');
 
 --
 -- Indexes for dumped tables
@@ -348,13 +342,13 @@ ALTER TABLE `category`
 -- AUTO_INCREMENT for table `customer`
 --
 ALTER TABLE `customer`
-  MODIFY `c_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `c_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `delivery`
 --
 ALTER TABLE `delivery`
-  MODIFY `d_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `d_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
 
 --
 -- AUTO_INCREMENT for table `food`
@@ -366,25 +360,25 @@ ALTER TABLE `food`
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `o_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `o_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
 
 --
 -- AUTO_INCREMENT for table `order_details`
 --
 ALTER TABLE `order_details`
-  MODIFY `od_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+  MODIFY `od_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=74;
 
 --
 -- AUTO_INCREMENT for table `payment`
 --
 ALTER TABLE `payment`
-  MODIFY `p_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `p_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- AUTO_INCREMENT for table `review`
 --
 ALTER TABLE `review`
-  MODIFY `r_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `r_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `tickets`
