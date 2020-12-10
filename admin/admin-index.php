f02a6c25e67e7cdd85e7bde76f8270cfc074cf10
Bhavesh Lohana <53433312+bhaveshlohana@users.noreply.github.com>
2020-12-10 19:09:44 +0530

    dashboard

--- admin/admin-index.php
+++ admin/admin-index.php
@@ -1,3 +1,11 @@
+<?php 
+    session_start();
+    error_reporting(0);
+    $conn=mysqli_connect("localhost", "bhavesh", "test123", "foodorderingsite");
+    if(!$conn) {
+        echo "Connection Error: " . mysqli_connect_error();
+    } 
+    ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -84,8 +92,16 @@
                                     <div class="row no-gutters align-items-center">
                                         <div class="col mr-2">
                                             <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                                Earnings (Monthly)</div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
+                                                Number of customers</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800"><?php 
+                                                $query="SELECT count(c_id) as customerCount from customer";
+                                            
+                                            $result = mysqli_query($conn, $query);
+
+                                            // fetch the resulting rows as an array
+                                            $row = mysqli_fetch_assoc($result);
+                                                echo $row['customerCount'];
+                                            ?></div>
                                         </div>
                                         <div class="col-auto">
                                             <i class="fas fa-calendar fa-2x text-gray-300"></i>
@@ -102,8 +118,18 @@
                                     <div class="row no-gutters align-items-center">
                                         <div class="col mr-2">
                                             <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                                                Earnings (Annual)</div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
+                                                Total Earnings(in ₹)</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800">₹
+                                                <?php 
+                                                $query="SELECT sum(o_price) as earningCount from orders";
+                                            
+                                                $result = mysqli_query($conn, $query);
+
+                                                // fetch the resulting rows as an array
+                                                $row = mysqli_fetch_assoc($result);
+                                                    echo $row['earningCount'];
+                                                ?>
+                                            </div>
                                         </div>
                                         <div class="col-auto">
                                             <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
@@ -119,19 +145,29 @@
                                 <div class="card-body">
                                     <div class="row no-gutters align-items-center">
                                         <div class="col mr-2">
-                                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Tasks
+                                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Orders Taken
                                             </div>
                                             <div class="row no-gutters align-items-center">
                                                 <div class="col-auto">
-                                                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">50%</div>
+                                                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">
+                                                        <?php 
+                                                $query="SELECT count(o_id) as orderCount from orders";
+                                            
+                                                $result = mysqli_query($conn, $query);
+
+                                                // fetch the resulting rows as an array
+                                                $row = mysqli_fetch_assoc($result);
+                                                    echo $row['orderCount'];
+                                                ?>
+                                                    </div>
                                                 </div>
-                                                <div class="col">
+                                                <!-- <div class="col">
                                                     <div class="progress progress-sm mr-2">
                                                         <div class="progress-bar bg-info" role="progressbar"
                                                             style="width: 50%" aria-valuenow="50" aria-valuemin="0"
                                                             aria-valuemax="100"></div>
                                                     </div>
-                                                </div>
+                                                </div> -->
                                             </div>
                                         </div>
                                         <div class="col-auto">
@@ -149,8 +185,18 @@
                                     <div class="row no-gutters align-items-center">
                                         <div class="col mr-2">
                                             <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                                                Pending Requests</div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
+                                                Reviews Received</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800">
+                                                <?php 
+                                                $query="SELECT count(r_id) as reviewCount from review";
+                                            
+                                                $result = mysqli_query($conn, $query);
+
+                                                // fetch the resulting rows as an array
+                                                $row = mysqli_fetch_assoc($result);
+                                                    echo $row['reviewCount'];
+                                                ?>
+                                            </div>
                                         </div>
                                         <div class="col-auto">
                                             <i class="fas fa-comments fa-2x text-gray-300"></i>
